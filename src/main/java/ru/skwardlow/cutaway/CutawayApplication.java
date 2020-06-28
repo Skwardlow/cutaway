8974f8f560d3f4dbf4c1e54e977f8a45305f16b8
Skwardlow <gorskovdmitry@gmail.com>
2020-06-28 16:07:08 +0700

    initial commmit

---
+++ src/main/java/ru/skwardlow/cutaway/CutawayApplication.java
@@ -0,0 +1,13 @@
+package ru.skwardlow.cutaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutawayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skwardlow/cutaway/CutawayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skwardlow.cutaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
